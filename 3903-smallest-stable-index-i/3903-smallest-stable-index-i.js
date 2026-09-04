63c01e01b69d372acfd74086205fbb6925c9fa9e
Sneha Gupta <guptaseema2108@gmail.com>
2026-09-04 09:53:38 +0530

    Time: 1 ms (96.3%), Space: 56.3 MB (62.96%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function(nums, k) {
+    let n = nums.length;
+
+    // suffixMin[i] = minimum from i to n-1
+    let suffixMin = new Array(n);
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+    }
+
+    let prefixMax = nums[0];
+
+    for (let i = 0; i < n; i++) {
+        prefixMax = Math.max(prefixMax, nums[i]);
+
+        if (prefixMax - suffixMin[i] <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
