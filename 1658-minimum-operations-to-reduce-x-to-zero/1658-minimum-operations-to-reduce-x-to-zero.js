bf778f24cb15e085888720499ecf4745ee7ddb8a
Sneha Gupta <guptaseema2108@gmail.com>
2026-09-23 12:27:36 +0530

    Time: 5 ms (71.43%), Space: 67.3 MB (28.57%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
@@ -0,0 +1,25 @@
+var minOperations = function(nums, x) {
+    const totalSum = nums.reduce((sum, num) => sum + num, 0);
+    const target = totalSum - x;
+    if (target < 0) return -1;
+    if (target === 0) return nums.length;
+    let left = 0;
+    let windowSum = 0;
+    let maxLength = -1;
+    for (let right = 0; right < nums.length; right++) {
+        windowSum += nums[right];    
+        while (windowSum > target && left <= right) {
+            windowSum -= nums[left];
+            left++;
+        }
+        if (windowSum === target) {
+            maxLength = Math.max(
+                maxLength,
+                right - left + 1
+            );
+        }
+    }
+    return maxLength === -1
+        ? -1
+        : nums.length - maxLength;
+};
